9f8d1bc7dbf132a8cf04d1894ca873dbba7c98ae
heruv1m <mUmLm15S>
2016-04-04 15:19:03 +0300

    testy2

--- src/test/java/com/scrumtrek/simplestore/CustomerTest.java
+++ src/test/java/com/scrumtrek/simplestore/CustomerTest.java
@@ -1,9 +1,5 @@
 package com.scrumtrek.simplestore;
 
-import com.scrumtrek.simplestore.Customer;
-import com.scrumtrek.simplestore.Movie;
-import com.scrumtrek.simplestore.PriceCodes;
-import com.scrumtrek.simplestore.Rental;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -15,20 +11,55 @@ public class CustomerTest {
     @Test
     public void testCustomerRegular(){
         String movieTitle = "mov-title";
-        String movieTitle2 = "mov-title2";
-        PriceCodes ps = PriceCodes.Regular;
+/*//        PriceCodes ps = PriceCodes.Regular;
         int days = 11;
         Movie m = new Movie(movieTitle, ps);
         String customerName = "some-name";
         Customer c = new Customer(customerName);
         Rental r = new Rental(m, days);
-//        c.addRental(r);
-        c.addRental(r);
+        c.addRental(r);*/
+        Customer c = createCustomer(PriceCodes.Regular, movieTitle);
         String result = c.Statement();
         System.out.println(result);
        String[] resultRows =  result.split("\\n");
         assertTrue(resultRows[1].contains(movieTitle));
         assertTrue(resultRows[1].contains("15.5"));
 
+    }
+
+    @Test
+    public void testCustomerChildren(){
+        String movieTitle = "mov-title";
+        Customer c = createCustomer(PriceCodes.Childrens, movieTitle);
+        String result = c.Statement();
+        System.out.println(result);
+        String[] resultRows =  result.split("\\n");
+        assertTrue(resultRows[1].contains(movieTitle));
+        assertTrue(resultRows[1].contains("12.0"));
+
+    }
+
+    @Test
+    public void testCustomerNewRelease(){
+        String movieTitle = "mov-title";
+        Customer c = createCustomer(PriceCodes.NewRelease, movieTitle);
+        String result = c.Statement();
+        System.out.println(result);
+        String[] resultRows =  result.split("\\n");
+        assertTrue(resultRows[1].contains(movieTitle));
+        assertTrue(resultRows[1].contains("33.0"));
+
+    }
+
+    public Customer createCustomer(PriceCodes ps, String movieTitle){
+//        String movieTitle = "mov-title";
+//        PriceCodes ps = PriceCodes.Regular;
+        int days = 11;
+        Movie m = new Movie(movieTitle, ps);
+        String customerName = "some-name";
+        Customer c = new Customer(customerName);
+        Rental r = new Rental(m, days);
+        c.addRental(r);
+        return c;
     }
 }
