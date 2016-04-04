830e9aaf47ed844ed500f1cc2b7b7a82f120b987
heruv1m <mUmLm15S>
2016-04-04 15:09:58 +0300

    testy2

--- src/test/java/com/scrumtrek/simplestore/CustomerTest.java
+++ src/test/java/com/scrumtrek/simplestore/CustomerTest.java
@@ -6,6 +6,8 @@ import com.scrumtrek.simplestore.PriceCodes;
 import com.scrumtrek.simplestore.Rental;
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by student on 04.04.2016.
  */
@@ -20,8 +22,13 @@ public class CustomerTest {
         String customerName = "some-name";
         Customer c = new Customer(customerName);
         Rental r = new Rental(m, days);
+//        c.addRental(r);
         c.addRental(r);
         String result = c.Statement();
         System.out.println(result);
+       String[] resultRows =  result.split("\\n");
+        assertTrue(resultRows[1].contains(movieTitle));
+        assertTrue(resultRows[1].contains("15.5"));
+
     }
 }
